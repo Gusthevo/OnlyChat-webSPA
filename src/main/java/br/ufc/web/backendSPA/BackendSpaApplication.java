c511a5d6d8f5861dc309a6c141f59493ad353622
Gusthevo <gustavocraft321@gmail.com>
2023-07-05 20:18:49 -0300

    Upando Commit

---
+++ src/main/java/br/ufc/web/backendSPA/BackendSpaApplication.java
@@ -0,0 +1,13 @@
+package br.ufc.web.backendSPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpaApplication.class, args);
+	}
+
+}
